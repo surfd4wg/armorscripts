5f5c360d5b6eeb06e796fbe08325dcda1f887d28
surfd4wg <63609504+surfd4wg@users.noreply.github.com>
2021-04-21 13:50:52 -0600

    Update checkservicesinstalled.ps1

--- checkservicesinstalled.ps1
+++ checkservicesinstalled.ps1
@@ -1,14 +1,14 @@
-# Check and to see if the following Armor related Windows services are installed
 [Array] $Services = 'Amazon SSM Agent','Armor Core','armor-filebeat','armor-winlogbeat','Qualys Cloud Agent','Trend Micro Deep Security Agent','Trend Micro Deep Security Monitor','Trend Micro Deep Security Notifier','Trend Micro Solution Platform','sshd','Remote Desktop Services';
 
-# loop through each service, if its not running, start it
-foreach($ServiceName in $Services)
-{
-	$Service = $ServiceName
-    	$arrService = Get-Service -Name $ServiceName
-	if ($arrService -eq- $null) {
-		Write-Host $ServiceName is NOT Installed
-	} else {
-		Write-Host $ServiceName is Installed
-	}
+$server="Localhost"
+function Confirm-WindowsServiceExists($name)
+{   
+   if (Get-Service -Name $name -Computername $server -ErrorAction SilentlyContinue)
+   {
+       Write-Host "$name Exists on $server"
+       return $true
+   }
+       Write-Host "$name does not exist on $server"
+       return $false
 }
+foreach($ServiceName in $Services) {Confirm-WindowsServiceExists($ServiceName)}
