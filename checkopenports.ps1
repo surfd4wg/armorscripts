6e1e36f3af95c0b310a6415f5c5e816dab929aa5
surfd4wg <63609504+surfd4wg@users.noreply.github.com>
2021-07-28 11:21:39 -0600

    Update checkopenports.ps1

--- checkopenports.ps1
+++ checkopenports.ps1
@@ -1,1 +1,2 @@
+# https://docs.armor.com/display/KBSS/Troubleshoot+Protection+Scores.mobile.phone
 tnc -computername 3a.epsec.armor.com -port 4119; tnc -computername 3a.epsec.armor.com -port 4120; tnc -computername 3a.epsec.armor.com -port 4122
